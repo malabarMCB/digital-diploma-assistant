d6878b916d2b5cdc00e5c3675605a5196806a7de
malabarMCB <grinevich97@gmail.com>
2019-04-12 19:43:55 +0300

    Create solution with Api

---
+++ src/DigitalDiplomaAssistant/Api/Controllers/HomeController.fs
@@ -0,0 +1,11 @@
+﻿namespace Api.Controllers
+
+open Microsoft.AspNetCore.Mvc
+
+[<Route("[controller]")>]
+type HomeController () =
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.Index() =
+        this.View()
