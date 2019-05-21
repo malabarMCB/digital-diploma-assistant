bcbe9e8e7bcf5b0dcd4960d6b139ec5f68dd7b04
malabarMCB <grinevich97@gmail.com>
2019-05-22 00:56:21 +0300

    Add dashboard front to view

--- src/DigitalDiplomaAssistant/Api/Controllers/DashboardController.fs
+++ src/DigitalDiplomaAssistant/Api/Controllers/DashboardController.fs
@@ -4,13 +4,15 @@ open Microsoft.AspNetCore.Mvc
 open Api
 open Microsoft.AspNetCore.Authorization
 open Microsoft.AspNetCore.Authentication.Cookies;
+open Microsoft.AspNetCore.Http
 
 [<Route("[controller]")>]
 [<Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)>]
-type DashboardController () =
+type DashboardController (httpContextAccessor: IHttpContextAccessor) =
     inherit Controller()
 
     [<HttpGet>]
     member this.Index() =
+        let user = httpContextAccessor.HttpContext.User
         let items = getDashboardTasks
         this.View(items)
