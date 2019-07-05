d1c48b0d328ab720789cdbdc79657129e0626b63
malabar <grinevich97@gmail.com>
2019-07-05 23:20:05 +0300

    Remove Queries and Commands from sln

--- src/DigitalDiplomaAssistant/Playground/Elastic.fs
+++ src/DigitalDiplomaAssistant/Playground/Elastic.fs
@@ -1,162 +1,162 @@
 ﻿namespace Tests
 
-open Common
-open System
-open NUnit.Framework
-open Nest
-open Queries
-open Queries.Dashboard
-open DataAccess
+//open Common
+//open System
+//open NUnit.Framework
+//open Nest
+//open Queries
+//open Queries.Dashboard
+//open DataAccess
 
-type Data = {
-        Name: string
-        Student: string
-        Assignee: string
-        Group: string
-        Status: string
-        Type: string
-}
+//type Data = {
+//        Name: string
+//        Student: string
+//        Assignee: string
+//        Group: string
+//        Status: string
+//        Type: string
+//}
 
-type A () = class end
-type B () = class end
+//type A () = class end
+//type B () = class end
 
-type Script<'T> = {
-    Lang: string
-    Inline: string
-    Params: 'T
-}
+//type Script<'T> = {
+//    Lang: string
+//    Inline: string
+//    Params: 'T
+//}
 
-type Update<'T> = {
-    Script: Script<'T>
-}
+//type Update<'T> = {
+//    Script: Script<'T>
+//}
 
-type CommentParam = {
-    Comment: Domain.PublicTypes.Comment
-}
+//type CommentParam = {
+//    Comment: Domain.PublicTypes.Comment
+//}
 
 
-[<TestClass>]
-type TestClass () =
-    let elasticUri = "Elastic:Uri" |> getEnvVariable
-    let elasticUserName = "Elastic:UserName" |> getEnvVariable
-    let elasticPassword = "Elastic:Password" |> getEnvVariable
+//[<TestClass>]
+//type TestClass () =
+//    let elasticUri = "Elastic:Uri" |> getEnvVariable
+//    let elasticUserName = "Elastic:UserName" |> getEnvVariable
+//    let elasticPassword = "Elastic:Password" |> getEnvVariable
 
-    [<Test>]
-    member __.ShouldGetDocsFromElastic () =
-        let node = new Uri(elasticUri)
-        let settings = new ConnectionSettings(node);
-        settings.BasicAuthentication(elasticUserName,elasticPassword) |> ignore
-        settings.EnableDebugMode(fun x -> x.DebugInformation |> printfn "Debug info %A") |> ignore
-        settings.DefaultIndex("dda") |> ignore
-        let client = new ElasticClient(settings);
+//    [<Test>]
+//    member __.ShouldGetDocsFromElastic () =
+//        let node = new Uri(elasticUri)
+//        let settings = new ConnectionSettings(node);
+//        settings.BasicAuthentication(elasticUserName,elasticPassword) |> ignore
+//        settings.EnableDebugMode(fun x -> x.DebugInformation |> printfn "Debug info %A") |> ignore
+//        settings.DefaultIndex("dda") |> ignore
+//        let client = new ElasticClient(settings);
         
-        //this works
-        let search1 = client.Search<Data>(fun (s:SearchDescriptor<Data>)  ->
-          new SearchRequest(
-            Query = new QueryContainer(query = BoolQuery(Should = [
-              new QueryContainer(query = new TermQuery(Field = new Field("filename"), Value = "a"));
-              new QueryContainer(query = new TermQuery(Field = new Field("contents"), Value = "b"))]))
-          ) :> ISearchRequest)
+//        //this works
+//        let search1 = client.Search<Data>(fun (s:SearchDescriptor<Data>)  ->
+//          new SearchRequest(
+//            Query = new QueryContainer(query = BoolQuery(Should = [
+//              new QueryContainer(query = new TermQuery(Field = new Field("filename"), Value = "a"));
+//              new QueryContainer(query = new TermQuery(Field = new Field("contents"), Value = "b"))]))
+//          ) :> ISearchRequest)
         
-        //this works
-        let search = client.Search<Data>(fun (s: SearchDescriptor<Data>) ->
-            SearchRequest(
-                                 Query = QueryContainer(MatchQuery(Field = Field("status"), Query ="in progress"))
-                             ) :> ISearchRequest)
+//        //this works
+//        let search = client.Search<Data>(fun (s: SearchDescriptor<Data>) ->
+//            SearchRequest(
+//                                 Query = QueryContainer(MatchQuery(Field = Field("status"), Query ="in progress"))
+//                             ) :> ISearchRequest)
         
-        //this works
-        let search2 = client.Search<Data> (fun (s : SearchDescriptor<Data>) ->
-            let indeces = Indices.Parse("dda")
-            let types = Types.Parse("task")
-            let request = SearchRequest(indeces, types)
-            request.Query <- QueryContainer(MatchAllQuery()) 
-            request :> ISearchRequest
-            )
+//        //this works
+//        let search2 = client.Search<Data> (fun (s : SearchDescriptor<Data>) ->
+//            let indeces = Indices.Parse("dda")
+//            let types = Types.Parse("task")
+//            let request = SearchRequest(indeces, types)
+//            request.Query <- QueryContainer(MatchAllQuery()) 
+//            request :> ISearchRequest
+//            )
         
-        search2.Documents |> printfn "%A"
-        ()
+//        search2.Documents |> printfn "%A"
+//        ()
 
-    [<Test>]
-    member __.ShouldReturnTasks () =
-        let node = new Uri(elasticUri)
-        let settings = new ConnectionSettings(node);
+//    [<Test>]
+//    member __.ShouldReturnTasks () =
+//        let node = new Uri(elasticUri)
+//        let settings = new ConnectionSettings(node);
 
-        //this compiles but does not work
-        //settings.DefaultMappingFor<DashboardQuery.Task>(FsNest.config<DashboardQuery.Task> (fun x -> x.IdProperty("Id") :> IClrTypeMapping<DashboardQuery.Task>)) |> ignore
+//        //this compiles but does not work
+//        //settings.DefaultMappingFor<DashboardQuery.Task>(FsNest.config<DashboardQuery.Task> (fun x -> x.IdProperty("Id") :> IClrTypeMapping<DashboardQuery.Task>)) |> ignore
 
-        settings.BasicAuthentication(elasticUserName, elasticPassword) |> ignore
-        settings.EnableDebugMode(fun x -> x.DebugInformation |> printfn "Debug info %A") |> ignore
-        settings.DefaultIndex("dda") |> ignore
-        let client = ElasticClient(settings);
+//        settings.BasicAuthentication(elasticUserName, elasticPassword) |> ignore
+//        settings.EnableDebugMode(fun x -> x.DebugInformation |> printfn "Debug info %A") |> ignore
+//        settings.DefaultIndex("dda") |> ignore
+//        let client = ElasticClient(settings);
 
-        let search = client |> FsNest.query<ElasticTask> "dda-task" (fun sd -> 
-            let query = QueryContainer(MatchAllQuery())
-            query
-            ) 
-        search.Hits |> Seq.cast<IHit<ElasticTask>> |> Seq.iter( fun x -> x.Source |> printfn "%A")
-        search.Documents |> printfn "%A"
-        ()
+//        let search = client |> FsNest.query<ElasticTask> "dda-task" (fun sd -> 
+//            let query = QueryContainer(MatchAllQuery())
+//            query
+//            ) 
+//        search.Hits |> Seq.cast<IHit<ElasticTask>> |> Seq.iter( fun x -> x.Source |> printfn "%A")
+//        search.Documents |> printfn "%A"
+//        ()
 
-    [<Test>]
-    member __.ShouldFindByKeyword () = 
-        let node = new Uri(elasticUri)
-        let settings = new ConnectionSettings(node);
-        settings.BasicAuthentication(elasticUserName,elasticPassword) |> ignore
-        settings.EnableDebugMode(fun x -> x.DebugInformation |> printfn "Debug info %A") |> ignore
-        settings.DefaultIndex("dda") |> ignore
-        let client = new ElasticClient(settings);
+//    [<Test>]
+//    member __.ShouldFindByKeyword () = 
+//        let node = new Uri(elasticUri)
+//        let settings = new ConnectionSettings(node);
+//        settings.BasicAuthentication(elasticUserName,elasticPassword) |> ignore
+//        settings.EnableDebugMode(fun x -> x.DebugInformation |> printfn "Debug info %A") |> ignore
+//        settings.DefaultIndex("dda") |> ignore
+//        let client = new ElasticClient(settings);
 
-        client
-        |> FsNest.query<ElasticTask> "dda-task" (fun sd -> 
-            QueryContainer(MatchQuery(Field = Field("type"), Query = "практики" ))
-        ) |> FsNest.hits<ElasticTask>
-        |> Seq.iter (fun x -> x.Source |> printfn "%A")
-        ()
+//        client
+//        |> FsNest.query<ElasticTask> "dda-task" (fun sd -> 
+//            QueryContainer(MatchQuery(Field = Field("type"), Query = "практики" ))
+//        ) |> FsNest.hits<ElasticTask>
+//        |> Seq.iter (fun x -> x.Source |> printfn "%A")
+//        ()
 
-    [<Test>]
-    member __.AddCommentScenario () =
-        let node = new Uri(elasticUri)
-        let settings = new ConnectionSettings(node);
-        settings.BasicAuthentication(elasticUserName,elasticPassword) |> ignore
-        let client = ElasticClient(settings)
+//    [<Test>]
+//    member __.AddCommentScenario () =
+//        let node = new Uri(elasticUri)
+//        let settings = new ConnectionSettings(node);
+//        settings.BasicAuthentication(elasticUserName,elasticPassword) |> ignore
+//        let client = ElasticClient(settings)
         
-        //this not work
-        //let indexName = IndexName.op_Implicit "dda-task"
-        //let typeName = TypeName.op_Implicit "_doc"
-        //let id = Id.op_Implicit ""
-        //let updateRequest = new UpdateRequest<Queries.Task.PublicTypes.ElasticTask, Queries.PublicTypes.Comment>(indexName, typeName, id)
-        //updateRequest.Script = new Scriptbas() :> IScript
-        //()
+//        //this not work
+//        //let indexName = IndexName.op_Implicit "dda-task"
+//        //let typeName = TypeName.op_Implicit "_doc"
+//        //let id = Id.op_Implicit ""
+//        //let updateRequest = new UpdateRequest<Queries.Task.PublicTypes.ElasticTask, Queries.PublicTypes.Comment>(indexName, typeName, id)
+//        //updateRequest.Script = new Scriptbas() :> IScript
+//        //()
 
-        //this works
-        //let postData = Elasticsearch.Net.PostData.op_Implicit "{\"script\":{\"lang\":\"painless\",\"inline\":\"ctx._source.comments.add(params.comment)\",\"params\":{\"comment\":{\"text\":\"yoo\"}}}}"
+//        //this works
+//        //let postData = Elasticsearch.Net.PostData.op_Implicit "{\"script\":{\"lang\":\"painless\",\"inline\":\"ctx._source.comments.add(params.comment)\",\"params\":{\"comment\":{\"text\":\"yoo\"}}}}"
 
-        //this works too
-        let update = {
-            Script = {
-                Lang = "painless"
-                Inline = "ctx._source.comments.add(params.comment)"
-                Params = {
-                    Comment = {
-                        Author= {
-                            Id = "test id"
-                            FirstName = "ddd"
-                            LastName = "bbb"
-                        }
-                        Text = "some text 4 comment"
-                        PostDate = System.DateTime.UtcNow
-                        Attachments = [
-                        {
-                            Name = "file1"
-                            FilePath = "C:"
-                            UploadDate = System.DateTime.UtcNow
-                        }
-                        ]
-                    }
-                }
-            } 
-        } 
+//        //this works too
+//        let update = {
+//            Script = {
+//                Lang = "painless"
+//                Inline = "ctx._source.comments.add(params.comment)"
+//                Params = {
+//                    Comment = {
+//                        Author= {
+//                            Id = "test id"
+//                            FirstName = "ddd"
+//                            LastName = "bbb"
+//                        }
+//                        Text = "some text 4 comment"
+//                        PostDate = System.DateTime.UtcNow
+//                        Attachments = [
+//                        {
+//                            Name = "file1"
+//                            FilePath = "C:"
+//                            UploadDate = System.DateTime.UtcNow
+//                        }
+//                        ]
+//                    }
+//                }
+//            } 
+//        } 
 
-        let postData = Elasticsearch.Net.PostData.Serializable update
-        let result = client.LowLevel.Update<Elasticsearch.Net.DynamicResponse>("dda-task", "_doc", "GJZPLGsB2N0LygMFURhw", postData)
-        ()
+//        let postData = Elasticsearch.Net.PostData.Serializable update
+//        let result = client.LowLevel.Update<Elasticsearch.Net.DynamicResponse>("dda-task", "_doc", "GJZPLGsB2N0LygMFURhw", postData)
+//        ()
