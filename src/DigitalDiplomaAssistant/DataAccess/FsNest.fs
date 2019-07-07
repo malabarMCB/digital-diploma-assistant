b4cb26c4ee1393cc85fbe68dfaa525946db2d3ce
malabar <grinevich97@gmail.com>
2019-07-07 17:29:41 +0300

    Add queries to FsNest

--- src/DigitalDiplomaAssistant/DataAccess/FsNest.fs
+++ src/DigitalDiplomaAssistant/DataAccess/FsNest.fs
@@ -22,39 +22,56 @@ module FsNest =
             request.Query <- f(s)
             request :> ISearchRequest
         )
+
+    let update: string -> string -> (UpdateRequest<_,_> -> unit) -> ElasticClient -> IUpdateResponse<_> = 
+        fun indexName id f elasticClient  -> 
+            let indexName = IndexName.op_Implicit indexName
+            let typeName = TypeName.op_Implicit "_doc"
+            let id = Id.op_Implicit id
+            let updateRequest = UpdateRequest(indexName, typeName, id)
+            f(updateRequest) |> ignore
+            elasticClient.Update<_,_>(updateRequest :> IUpdateRequest<_,_>)
+
+    let updateByQuery: string -> (UpdateByQueryRequest -> unit) -> ElasticClient -> IUpdateByQueryResponse = 
+        fun indexName f elasticClient -> 
+            let indexName = indexName |> IndexName.op_Implicit |>Indices.op_Implicit
+            let typeName = "_doc" |> TypeName.op_Implicit  |> Types.op_Implicit
+            let updateRequest = UpdateByQueryRequest(indexName, typeName)
+            f(updateRequest) |> ignore
+            elasticClient.UpdateByQuery(updateRequest)
   
-    type Script<'T> = {
-        Lang: string
-        Inline: string
-        Params: 'T
-    }
+    //type Script<'T> = {
+    //    Lang: string
+    //    Inline: string
+    //    Params: 'T
+    //}
 
-    type Update<'T> = {
-        Script: Script<'T>
-    }
+    //type Update<'T> = {
+    //    Script: Script<'T>
+    //}
 
-    type Query<'T> = {
-        Query: 'T
-    }
+    //type Query<'T> = {
+    //    Query: 'T
+    //}
 
-    type UpdateByQuery<'T, 'U> = {
-        Query: 'T
-        Script: Script<'U>
-    }
+    //type UpdateByQuery<'T, 'U> = {
+    //    Query: 'T
+    //    Script: Script<'U>
+    //}
 
-    let update indexName id (script: Script<'T>) (elasticClient: ElasticClient)= 
-        let update = {Script = script}
-        let postData = Elasticsearch.Net.PostData.Serializable update
-        let response = elasticClient.LowLevel.Update<Elasticsearch.Net.DynamicResponse>(indexName, "_doc", id, postData)
-        elasticClient.Refresh (Indices.Parse indexName) |> ignore
-        response
+    //let update indexName id (script: Script<'T>) (elasticClient: ElasticClient)= 
+    //    let update = {Script = script}
+    //    let postData = Elasticsearch.Net.PostData.Serializable update
+    //    let response = elasticClient.LowLevel.Update<Elasticsearch.Net.DynamicResponse>(indexName, "_doc", id, postData)
+    //    elasticClient.Refresh (Indices.Parse indexName) |> ignore
+    //    response
 
-    let updateByQuery (elasticClient: ElasticClient) indexName (query: Query<'T>) (script: Script<'U>) = 
-        let update = {
-            Query = query.Query
-            Script = script
-        }
-        let postData = Elasticsearch.Net.PostData.Serializable update
-        let response = elasticClient.LowLevel.UpdateByQuery<Elasticsearch.Net.DynamicResponse>(indexName, "_doc", postData)
-        elasticClient.Refresh (Indices.Parse indexName) |> ignore
-        response
+    //let updateByQuery (elasticClient: ElasticClient) indexName (query: Query<'T>) (script: Script<'U>) = 
+    //    let update = {
+    //        Query = query.Query
+    //        Script = script
+    //    }
+    //    let postData = Elasticsearch.Net.PostData.Serializable update
+    //    let response = elasticClient.LowLevel.UpdateByQuery<Elasticsearch.Net.DynamicResponse>(indexName, "_doc", postData)
+    //    elasticClient.Refresh (Indices.Parse indexName) |> ignore
+    //    response
--- src/DigitalDiplomaAssistant/DataAccess/Task/TaskCommands.fs
+++ src/DigitalDiplomaAssistant/DataAccess/Task/TaskCommands.fs
@@ -1,8 +1,9 @@
 ﻿namespace DataAccess.Task
 
+open System.Collections.Generic
 open DataAccess
 open Domain
-open FsNest
+open Nest
 
 module Commands = 
     type  StatusParams = {
@@ -11,29 +12,50 @@ module Commands =
     }
     let updateTaskStatus: ElasticOptions -> UpdateTaskStatusInDb = 
         fun elasticOptions id status assignee -> 
-            elasticOptions |> FsNest.createElasticClient
-            |> FsNest.update "dda-task" id {
-                Lang = "painless"
-                Inline = "ctx._source.status = params.status; ctx._source.assignee = params.assignee"
-                Params = {
-                    Status = status.ToString()
-                    Assignee = assignee
-                }
-             } |> ignore
+            //elasticOptions |> FsNest.createElasticClient
+            //|> FsNest.update "dda-task" id {
+            //    Lang = "painless"
+            //    Inline = "ctx._source.status = params.status; ctx._source.assignee = params.assignee"
+            //    Params = {
+            //        Status = status.ToString()
+            //        Assignee = assignee
+            //    }
+            // } |> ignore
+            elasticOptions
+            |> FsNest.createElasticClient
+            |> FsNest.update "dda-task" id (fun request -> 
+                let script = InlineScript("ctx._source.status = params.status; ctx._source.assignee = params.assignee")
+                script.Lang <- "painless"
+                script.Params <- Dictionary<_,_>()
+                script.Params.Add("status", status.ToString())
+                script.Params.Add("assignee", assignee)
+                request.Script <- script
+            )
+            |> ignore
     
     type CommentParams = {
         Comment: Comment
     }
     let addComment: ElasticOptions -> AddCommentToDb = 
         fun elasticOptions id comment -> 
-            elasticOptions |> FsNest.createElasticClient
-            |> FsNest.update "dda-task" id {
-                Lang = "painless"
-                Inline = "ctx._source.comments.add(params.comment)"
-                Params= {
-                    Comment = comment
-                }
-            } |> ignore
+            //elasticOptions |> FsNest.createElasticClient
+            //|> FsNest.update "dda-task" id {
+            //    Lang = "painless"
+            //    Inline = "ctx._source.comments.add(params.comment)"
+            //    Params= {
+            //        Comment = comment
+            //    }
+            //} |> ignore
+            elasticOptions
+            |> FsNest.createElasticClient
+            |> FsNest.update "dda-task" id (fun request -> 
+                let script = InlineScript("ctx._source.comments.add(params.comment)")
+                script.Lang <- "painless"
+                script.Params <- Dictionary<_,_>()
+                script.Params.Add("comment", comment)
+                request.Script <- script
+            )
+            |> ignore
 
     //metodist
     type ElasticAttachment = {
@@ -70,54 +92,83 @@ module Commands =
     //let deleteAttachmentDescription (elasticOptions: ElasticOptions) (attachment: Attachment) (taskType: TaskType) =
     let deleteAttachmentDescription: ElasticOptions ->  DeleteDescriptionAttachmentFromDb =
         fun elasticOptions attachment taskType -> 
-            let query = {
-                Query = {
-                    Term = {
-                        Type = {
-                          Value = TaskType.toString taskType
-                        }             
-                    }
-                }
-            }
-            let script: FsNest.Script<AttachmentParams> = {
-                Lang = "painless"
-                Inline = "ctx._source.description.attachments.remove(ctx._source.description.attachments.indexOf(params.attachment))"
-                Params= {
-                    Attachment = {
-                        Name = attachment.Name
-                        FilePath = attachment.FilePath
-                        UploadDate = attachment.UploadDate.ToString "yyyy-MM-dd"
-                    }
-                }
-            }
-            let client = elasticOptions |> FsNest.createElasticClient
-            FsNest.updateByQuery client "dda-task" query script |> ignore
-
-    let updateTaskDesctiption (elasticOptions: ElasticOptions) (taskType: TaskType) (description: Description) = 
-        let toElasticAttachment (attachment: Attachment) =
-            {
-                Name = attachment.Name
-                FilePath = attachment.FilePath
-                UploadDate = attachment.UploadDate.ToString "yyyy-MM-dd"
-            }
-        let query = {
-            Query = {
-                Term = {
-                    Type = {
-                        Value = TaskType.toString taskType
-                    }             
-                }
-            }
-        }
-        let script: FsNest.Script<DescriptionParams> = {
-            Lang = "painless"
-            Inline = "ctx._source.description = params.description"
-            Params= {
-                Description = {
-                    Text = description.Text
-                    Attachments = List.map toElasticAttachment description.Attachments
-                }
-            }
-        }
-        let client = elasticOptions |> FsNest.createElasticClient
-        FsNest.updateByQuery client "dda-task" query script |> ignore
+        //    let query = {
+        //        Query = {
+        //            Term = {
+        //                Type = {
+        //                  Value = TaskType.toString taskType
+        //                }             
+        //            }
+        //        }
+        //    }
+        //    let script: FsNest.Script<AttachmentParams> = {
+        //        Lang = "painless"
+        //        Inline = "ctx._source.description.attachments.remove(ctx._source.description.attachments.indexOf(params.attachment))"
+        //        Params= {
+        //            Attachment = {
+        //                Name = attachment.Name
+        //                FilePath = attachment.FilePath
+        //                UploadDate = attachment.UploadDate.ToString "yyyy-MM-dd"
+        //            }
+        //        }
+        //    }
+        //    let client = elasticOptions |> FsNest.createElasticClient
+        //    FsNest.updateByQuery client "dda-task" query script |> ignore
+            elasticOptions
+            |> FsNest.createElasticClient
+            |> FsNest.updateByQuery "dda-task" (fun request -> 
+                let query = QueryContainer(TermQuery(Field = Field("taskType"), Value = TaskType.toString taskType))
+                let script = InlineScript("ctx._source.description.attachments.remove(ctx._source.description.attachments.indexOf(params.attachment))")
+                script.Lang <- "painless"
+                script.Params <- Dictionary<_,_>()
+                let param = {|Name = attachment.Name; FilePath = attachment.FilePath; UploadDate = attachment.UploadDate.ToString "yyyy-MM-dd"|}
+                script.Params.Add("attachment", param)
+                request.Script <- script
+                request.Query <- query
+            )
+            |> ignore
+    let updateTaskDesctiption: ElasticOptions -> TaskType -> Description -> unit = 
+        fun elasticOptions taskType description ->
+        //let toElasticAttachment (attachment: Attachment) =
+        //    {
+        //        Name = attachment.Name
+        //        FilePath = attachment.FilePath
+        //        UploadDate = attachment.UploadDate.ToString "yyyy-MM-dd"
+        //    }
+        //let query = {
+        //    Query = {
+        //        Term = {
+        //            Type = {
+        //                Value = TaskType.toString taskType
+        //            }             
+        //        }
+        //    }
+        //}
+        //let script: FsNest.Script<DescriptionParams> = {
+        //    Lang = "painless"
+        //    Inline = "ctx._source.description = params.description"
+        //    Params= {
+        //        Description = {
+        //            Text = description.Text
+        //            Attachments = List.map toElasticAttachment description.Attachments
+        //        }
+        //    }
+        //}
+        //let client = elasticOptions |> FsNest.createElasticClient
+        //FsNest.updateByQuery client "dda-task" query script |> ignore
+        elasticOptions
+        |> FsNest.createElasticClient
+        |> FsNest.updateByQuery "dda-task" (fun request -> 
+            let query = QueryContainer(TermQuery(Field = Field("taskType"), Value = TaskType.toString taskType))
+            let script = InlineScript("ctx._source.description = params.description")
+            script.Lang <- "painless"
+            script.Params <- Dictionary<_,_>()
+            let attachments = description.Attachments |> List.map (fun (attachment: Domain.Attachment) -> 
+                {|Name = attachment.Name; FilePath = attachment.FilePath; UploadDate = attachment.UploadDate.ToString "yyyy-MM-dd"|}
+            )
+            let param = {|Text = description.Text; Attachments = attachments|}
+            script.Params.Add("description", param)
+            request.Query <- query
+            request.Script <- script
+        )
+        |> ignore
